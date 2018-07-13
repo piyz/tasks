c064a2f9b9deac95a16c8142339b7f897e4aab81
Andrey Matrosov <matrosovs2008@yandex.ru>
2018-07-13 21:37:18 +0300

    print level order traversal line by line

--- src/main/java/by/matrosov/bst/def/BinaryTree.java
+++ src/main/java/by/matrosov/bst/def/BinaryTree.java
@@ -3,7 +3,7 @@ package by.matrosov.bst.def;
 public class BinaryTree {
     private Node root;
 
-    public BinaryTree() {
+    private BinaryTree() {
         root = null;
     }
 
@@ -47,21 +47,64 @@ public class BinaryTree {
     }
 
     private static void initTree(BinaryTree tree){
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
+        tree.root = new Node(20);
+        tree.root.left = new Node(8);
+        tree.root.right = new Node(22);
         tree.root.left.left = new Node(4);
-        tree.root.left.right = new Node(5);
+        tree.root.left.right = new Node(12);
+        tree.root.left.right.left = new Node(10);
+        tree.root.left.right.right = new Node(14);
+    }
+
+    private void printAllLevels(){
+        printAllLevels(root);
+    }
+
+    private void printAllLevels(Node root){
+        int h = height(root);
+        for (int i = 1; i < h + 1; i++) {
+            printLevel(root, i);
+            System.out.println();
+        }
+    }
+
+    private int height(Node root) {
+        if (root == null){
+            return 0;
+        }else {
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+
+            if (leftHeight > rightHeight){
+                return leftHeight + 1;
+            }else {
+                return rightHeight + 1;
+            }
+        }
+    }
+
+    private void printLevel(Node root, int lvl) {
+        if (root == null){
+            return;
+        }
+        if (lvl == 1){
+            System.out.println(root.value);
+        }else if (lvl > 1){
+            printLevel(root.left, lvl - 1);
+            printLevel(root.right, lvl - 1);
+        }
     }
 
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
         initTree(tree);
 
-        tree.printPostOrder();
-        System.out.println("\n");
-        tree.printInOrder();
-        System.out.println("\n");
-        tree.printPreOrder();
+        //tree.printPostOrder();
+        //System.out.println("\n");
+        //tree.printInOrder();
+        //System.out.println("\n");
+        //tree.printPreOrder();
+
+        tree.printAllLevels();
     }
 }
