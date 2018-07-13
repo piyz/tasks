950f79f75d6618d21f8ad0591b481b647f6c9c0b
Andrey Matrosov <matrosovs2008@yandex.ru>
2018-07-13 17:35:16 +0300

    LinkedList fix
    add possibility to find indices of max subarray

--- src/main/java/by/matrosov/algorithms/kadane/Kadane.java
+++ src/main/java/by/matrosov/algorithms/kadane/Kadane.java
@@ -1,5 +1,7 @@
 package by.matrosov.algorithms.kadane;
 
+import java.util.LinkedList;
+
 /**
  * Largest Sum Contiguous Subarray
  */
@@ -7,21 +9,28 @@ public class Kadane {
     public static void main(String[] args) {
         int [] input = {-2, -3, 4, -1, -2, 1, 5, -3};
 
+        LinkedList<Integer> list = new LinkedList<>();
         int maxSoFar = 0;
         int maxEndingHere = 0;
 
-        for (int a : input) {
-            maxEndingHere = maxEndingHere + a;
+        for (int i = 0; i < input.length; i++) {
+            list.add(i);
+
+            maxEndingHere = maxEndingHere + input[i];
 
             if (maxEndingHere < 0){
                 maxEndingHere = 0;
+                list.clear();
             }
 
             if (maxSoFar < maxEndingHere){
                 maxSoFar = maxEndingHere;
+            }else {
+                if (!list.isEmpty())
+                    list.removeLast();
             }
         }
-
+        System.out.println(list);
         System.out.println(maxSoFar); // 4 -1 -2 1 5 = 7
     }
 }
