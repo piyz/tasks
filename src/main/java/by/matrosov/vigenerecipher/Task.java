ac9ca82e1f42ccbaf616b9973ea9f6c53dc1b391
Andrey Matrosov <matrosovs2008@yandex.ru>
2018-07-18 15:57:09 +0300

    add task to vigenere cipher

---
+++ src/main/java/by/matrosov/vigenerecipher/Task.java
@@ -0,0 +1,24 @@
+package by.matrosov.vigenerecipher;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Task {
+    public static void main(String[] args) {
+        int[] arr2 = {3, 2, 5, 1, 0, 4};
+        int[] arr1 = {40, 20, 5, 26, 25, 28};
+
+        Optional<String> sequence = Stream.iterate(0, i -> arr2[i])
+                .map(res -> arr1[res])
+                .skip(1)
+                .limit(arr2.length)
+                .map(Object::toString)
+                .reduce((a, b) -> a + ", " + b);
+
+        if (sequence.isPresent()){
+            sequence.map(s -> new StringBuilder(s).reverse().toString());
+        }
+
+        System.out.println(String.format("%s-%s-%s-%s-%s-%s", sequence.get().split(",")));
+    }
+}
--- src/main/java/by/matrosov/vigenerecipher/Vigenere.java
+++ src/main/java/by/matrosov/vigenerecipher/Vigenere.java
@@ -60,22 +60,4 @@ public class Vigenere {
             System.out.print(table.get(0).charAt(index));
         }
     }
-
-    //int[] arr2 = {3, 2, 5, 1, 0, 4};
-    //int[] arr1 = {40, 20, 5, 26, 25, 28};
-    //
-    //        Optional<String> sequence = Stream.iterate(0, i -> arr2[i])
-    //                .map(res -> arr1[res])
-    //                .skip(1)
-    //                .limit(arr2.length)
-    //                .map(Object::toString)
-    //                .reduce((a, b) -> a + ", " + b);
-    //
-    //        if (sequence.isPresent()){
-    //            sequence.map(s -> new StringBuilder(s).reverse().toString());
-    //        }
-    //
-    //        System.out.println(String.format("%s-%s-%s-%s-%s-%s", sequence.get().split(",")));
-    //
-    //
 }
